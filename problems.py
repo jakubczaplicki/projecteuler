bcefeb9a4d56b9660b9bacff121c190a114d2776
Jakub Czaplicki <jakub@broadcom.com>
2014-07-29 16:28:00 +0100

    add unfinished 24

--- problems.py
+++ problems.py
@@ -155,10 +155,45 @@ class problem6():
     sumB = sumB * sumB
     return sumB - sumA
     #25164150
+
+#A permutation is an ordered arrangement of objects. For example, 3124 is one
+#possible permutation of the digits 1, 2, 3 and 4. If all of the permutations
+#are listed numerically or alphabetically, we call it lexicographic order.
+# The lexicographic permutations of 0, 1 and 2 are:
+# 012   021   102   120   201   210
+#What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+class problem24():
+  import math
+  arr = [0,0,0]
+
+  def increase(self,n):
+    if ((self.arr[n]+1)%3 == 0) and ((n+1) < len(self.arr)):
+      self.arr[n] = 0
+      self.increase(n+1)
+    else:
+      self.arr[n] = self.arr[n] + 1
+      if (self.arr[n] > max(self.arr)):
+        self.arr[n] = 0
+      #elif (self.arr[n]) in ( self.arr[:n] + self.arr[(n+1):] ):
+      #  self.increase(n)
+
+    out = 0
+    for i in xrange(len(self.arr)):
+      out = out + self.arr[i] * 10**i
     
+  def result(self):
+    arrlen = len(self.arr)
+    for i in xrange( arrlen ** 3 ): #math.factorial(len(self.arr)) ):
+      print self.arr
+      self.increase(0)
+  ##unfinished
+
+
 #print problem1().result()
 #print problem2().result()
 #print problem3().result()
 #print problem4().result()
 #print problem5().result()
-print problem6().result()
+#print problem6().result()
+problem24().result()
+
